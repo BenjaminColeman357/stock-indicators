7bfb45d0d83422a90892e75148cc04c27748755f
BenjaminColeman357 <73182429+BenjaminColeman357@users.noreply.github.com>
2022-01-26 21:46:01 +0300

    Update indicator.test.js
    kdj

--- indicator.test.js
+++ indicator.test.js
@@ -9,10 +9,10 @@ var Indicator = require('./indicator.js');
 //   console.log(Indicator.MACD(testArray));
 // });
 
-// testArray = [[9.03, 7.52, 9.03], [9.93, 9.93, 9.93], [10.92, 10.92, 10.92], [12.01, 11.20, 12.01], [11.80, 11.28, 11.46], [12.49, 11.38, 12.19]];
-// describe('kdj test', function(){
-//   console.log(Indicator.KDJ(testArray));
-// });
+ testArray = [[9.03, 7.52, 9.03], [9.93, 9.93, 9.93], [10.92, 10.92, 10.92], [12.01, 11.20, 12.01], [11.80, 11.28, 11.46], [12.49, 11.38, 12.19]];
+ describe('kdj test', function(){
+   console.log(Indicator.KDJ(testArray));
+ });
 
 // testArray = [9.03, 9.93, 10.92, 12.01, 11.46, 12.19, 12.68, 12.39, 13.53, 14.36, 14.26, 13.71, 13.66, 13.24, 13.03, 13.14, 12.68, 12.37, 12.12, 12.18, 12.2, 12.38, 12.01, 11.81, 11.59];
 // describe('boll test', function(){
@@ -25,7 +25,7 @@ var Indicator = require('./indicator.js');
 //   console.log(Indicator.RSI(testArray));
 // });
 
-testArray = [9.03, 9.03, 10.92, 12.01, 11.46, 12.19, 12.68];
-describe('ma test', function(){
-  console.log(Indicator.MA(testArray, 20));
+//testArray = [9.03, 9.03, 10.92, 12.01, 11.46, 12.19, 12.68];
+//describe('ma test', function(){
+ // console.log(Indicator.MA(testArray, 20));
 });
